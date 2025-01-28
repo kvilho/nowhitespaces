f6261ae23ec697240a2e97c1c423677cd797cb4e
villekotilainen <bhi087@myy.haaga-helia.fi>
2025-01-28 09:51:48 +0200

    Entity Entry.java created

--- backend/src/main/java/fi/haagahelia/backend/model/Entry.java
+++ backend/src/main/java/fi/haagahelia/backend/model/Entry.java
@@ -1,5 +1,119 @@
 package fi.haagahelia.backend.model;
 
+import java.time.LocalDateTime;
+
+import fi.haagahelia.backend.model.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Entry {
+
+    enum Status {
+        APPROVED,
+        PENDING,
+        DECLINED
+    }
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long entryId;
+    private LocalDateTime entryStart;
+    private LocalDateTime entryEnd;
+    private Status entryStatus;
+    private String entryDescription;
+
+    @ManyToOne
+    @JoinColumn(name = "userId") //many-to-one relationship with User
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "organizationId") //many-to-one relationship with Organization
+    private Organization organization;
+
+    public Entry(long entryId, LocalDateTime entryStart, LocalDateTime entryEnd, Status entryStatus, String entryDescription) {
+        this.entryId = entryId;
+        this.entryStart = entryStart;
+        this.entryEnd = entryEnd;
+        this.entryStatus = entryStatus;
+        this.entryDescription = entryDescription;
+        this.user = user;
+        this.organization = organization;
+    }
+
+    public Entry() {
+    }
+
+    public Long getEntryId() {
+        return entryId;
+    }
+
+    public void setEntryId(Long entryId) {
+        this.entryId = entryId;
+    }
+
+    public LocalDateTime getEntryStart() {
+        return entryStart;
+    }
+
+    public void setEntryStart(LocalDateTime entryStart) {
+        this.entryStart = entryStart;
+    }
+
+    public LocalDateTime getEntryEnd() {
+        return entryEnd;
+    }
+
+    public void setEntryEnd(LocalDateTime entryEnd) {
+        this.entryEnd = entryEnd;
+    }
+
+    public Status getEntryStatus() {
+        return entryStatus;
+    }
+
+    public void setEntryStatus(Status entryStatus) {
+        this.entryStatus = entryStatus;
+    }
+
+    public String getEntryDescription() {
+        return entryDescription;
+    }
+
+    public void setEntryDescription(String entryDescription) {
+        this.entryDescription = entryDescription;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry [entryId=" + entryId + ", entryStart=" + entryStart + ", entryEnd=" + entryEnd + ", entryStatus="
+                + entryStatus + ", entryDescription=" + entryDescription + ", user=" + user + ", organization="
+                + organization + "]";
+    }
+    
+    
+
+
+
 }
