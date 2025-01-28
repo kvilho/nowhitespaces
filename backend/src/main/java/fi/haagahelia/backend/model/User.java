4d9529bea2410e3a94b62fecad5f7e2e714e13e6
bhi049 <bhi049@myy.haaga-helia.fi>
2025-01-28 08:55:20 +0200

    Entity classes started

---
+++ backend/src/main/java/fi/haagahelia/backend/model/User.java
@@ -0,0 +1,12 @@
+package fi.haagahelia.backend.model;
+
+import jakarta.persistence.*;
+
+public class User {
+
+    private String user_id;
+    private String firstname;
+    private String lastname;
+
+
+}
