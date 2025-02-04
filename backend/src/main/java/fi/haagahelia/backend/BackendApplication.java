ae90f7c814defbb2b1553d9f4a154512e3c18390
bhi053 <bhi053@myy.haaga-helia.fi>
2025-02-04 10:44:58 +0200

    demo data

--- backend/src/main/java/fi/haagahelia/backend/BackendApplication.java
+++ backend/src/main/java/fi/haagahelia/backend/BackendApplication.java
@@ -1,13 +1,47 @@
 package fi.haagahelia.backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.backend.model.Organization;
+import fi.haagahelia.backend.model.Role;
+import fi.haagahelia.backend.repositories.OrganizationRepository;
+import fi.haagahelia.backend.repositories.RoleRepository;
+import fi.haagahelia.backend.repositories.UserRepository;
 
 @SpringBootApplication
 public class BackendApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BackendApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+    public CommandLineRunner createStarterData(
+		UserRepository userRepository,
+		OrganizationRepository organizationRepository,
+		RoleRepository roleRepository) {
+
+		return (args) -> {
+			log.info("Creating initial data...");
+
+			// Organization
+			Organization organization1 = new Organization(null, "PajaRy");
+			organizationRepository.save(organization1);
+			Organization organization2 = new Organization(null, "Hirvi and the Headlights");
+			organizationRepository.save(organization2);
+
+			// Role
+			Role role1 = new Role(null, "Admin", "Administrator");
+			roleRepository.save(role1);
+
+			log.info("Sample data created successfully.");
+		};
+	}
 }
