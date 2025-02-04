089bac52c6b095054431b4139c2fd4d659cea84e
bhi049 <bhi049@myy.haaga-helia.fi>
2025-02-04 09:32:27 +0200

    Basic Home and Login pages with navbar

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Typography variant="h3" mt={5}>
+        Welcome to the Home Page
+      </Typography>
+    </Container>
+  );
+};
+
+export default Home;
