0f0dd3f87691be7fd45fbf0960b313a42eadbc9c
bhi053 <bhi053@myy.haaga-helia.fi>
2025-02-11 09:47:54 +0200

    security

--- backend/src/main/java/fi/haagahelia/backend/repositories/UserRepository.java
+++ backend/src/main/java/fi/haagahelia/backend/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package fi.haagahelia.backend.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import fi.haagahelia.backend.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
     
 }
