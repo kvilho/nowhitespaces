cc56c59c7fa65744d538630f00ccd5b36cb9a66d
bhi053 <bhi053@myy.haaga-helia.fi>
2025-02-12 13:06:43 +0200

    h2withoutlogin

--- backend/src/main/java/fi/haagahelia/backend/config/SecurityConfig.java
+++ backend/src/main/java/fi/haagahelia/backend/config/SecurityConfig.java
@@ -38,19 +38,21 @@ public class SecurityConfig {
     @Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests(authorize -> authorize
+		http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
 			.requestMatchers("/css/**", "/h2-console/**", "/api/**").permitAll()
 			.anyRequest().authenticated()
 		)
 		.formLogin(formlogin -> formlogin
-            // .loginPage("/login") // Custom login page
-			// .defaultSuccessUrl("/homepage", true) // Redirect after successful login
+            //.loginPage("/login") // Custom login page
+		    //.defaultSuccessUrl("/homepage", true) // Redirect after successful login
             .permitAll()
 		)
 		.logout(logout -> logout
-            // .logoutSuccessUrl("/") // Redirect after logout 
-			.permitAll()
-		);	
+            //.logoutSuccessUrl("/") // Redirect after logout 
+			.permitAll())
+        .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())); // Allow frames for H2 Console
 		return http.build();
 	}
 }
