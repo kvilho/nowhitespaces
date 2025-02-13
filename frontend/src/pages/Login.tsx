92593c47400286521cc512eed663768789f367fe
bhi049 <bhi049@myy.haaga-helia.fi>
2025-02-13 12:19:01 +0200

    Login form updated and new username input field

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -13,22 +13,28 @@ const Login: React.FC = () => {
     };
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', width: '300px' }}>
+        <div className="login-container">
+            <form onSubmit={handleSubmit} className="login-form">
                 <h2>Login</h2>
-                <label>
-                    Username:
+
+                {/* Username Field with Icon */}
+                <div className='input-field'>
+                    <svg className="icon" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 12c2.485 0 4.5-2.015 4.5-4.5S14.485 3 12 3 7.5 5.015 7.5 7.5 9.515 12 12 12zm0 2.25c-3.735 0-6.75 3.015-6.75 6.75h13.5c0-3.735-3.015-6.75-6.75-6.75z"></path>
+                    </svg>
                     <input
+                        className='input text-input'
+                        placeholder='Username'
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
-                        style={{ marginBottom: '10px', padding: '8px' }}
                     />
-                </label>
-                <div className='login-field'>
+                </div>
+
+                {/* Password Field with Icon */}
+                <div className='input-field'>
                     <svg className="icon" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" 
-                        strokeLinejoin="round" strokeLinecap="round"></path>
+                        <path d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" strokeLinejoin="round" strokeLinecap="round"></path>
                     </svg>
                     <input
                         className='input password-input'
@@ -38,7 +44,8 @@ const Login: React.FC = () => {
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-                <button type="submit" style={{ padding: '10px', backgroundColor: 'blue', color: 'white', border: 'none' }}>
+
+                <button type="submit" className="login-button">
                     Login
                 </button>
             </form>
